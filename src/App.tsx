4f7730b4904d84d6187e26c502c31c264a743eaa
He_vajno <111884984+Azema05@users.noreply.github.com>
2025-06-11 08:22:37 +0000

    mod3tsk1(2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-// src/App.tsx
-
 import React from 'react';
-import Message from './components/Message'; // Импортируем наш новый компонент
+import Message from './components/Message';
 
 const App: React.FC = () => {
-  // Создаем константу с текстом
-  const messageText = 'Привет! Это сообщение передано через пропс.';
+  const messageText = 'Пропс'
 
   return (
     <div>
       <h1>Моё приложение на Vite + React + TypeScript</h1>
-
-      {/* Рендерим компонент Message и передаем текст через пропс */}
+      {}
       <Message text={messageText} />
     </div>
   );
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,13 +1,7 @@
-// src/components/Message.tsx
-
 import React from 'react';
-
-// Определяем тип для пропсов — у нас будет одно поле "text" типа string
 type MessageProps = {
   text: string;
 };
-
-// Компонент Message получает props и выводит текст
 const Message: React.FC<MessageProps> = ({ text }) => {
   return (
     <div>
